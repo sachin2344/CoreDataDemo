01586aa5aa730649ad015cc04d8757134ad7082e
Sachin sharma <sachinsharma2344@gmail.com>
2020-07-15 01:30:55 +0530

    added person entity and some attributes

---
+++ CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by sachin sharma on 15/07/20.
+//  Copyright © 2020 Sachin Sharma. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
