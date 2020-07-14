42d036db0720737ff267e53c79b9a9be48c0d2f6
Sachin sharma <sachinsharma2344@gmail.com>
2020-07-15 00:37:20 +0530

    intial commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by sachin sharma on 15/07/20.
+//  Copyright © 2020 Sachin Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
